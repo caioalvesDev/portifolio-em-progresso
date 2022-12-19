a5be14affaae1c8f65d1e6fe18634f007b9e3882
Caio Alves <kaio_rx@hotmail.com>
2022-12-19 14:15:01 -0300

    upgrade em portfolio

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('dialog')
+const open_modal = document.querySelector('#sobre-modal')
+const close_modal = document.createElement('button')
+modal.appendChild(close_modal).innerHTML = 'fechar'.toUpperCase()
+
+
+
+open_modal.addEventListener('click', (e) => {
+    modal.showModal()
+})
+close_modal.onclick = () => {
+    modal.close()
+}
