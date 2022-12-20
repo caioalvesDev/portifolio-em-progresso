e4bb2f0b3bd6fcbfec7d98294762d03cf04d4fe5
Caio Alves <kaio_rx@hotmail.com>
2022-12-20 13:42:58 -0300

    ajustando cards

--- javascript.js
+++ javascript.js
@@ -1,14 +1,12 @@
 const modal = document.querySelector('dialog')
 const open_modal = document.querySelector('#sobre-modal')
-const close_modal = document.createElement('button')
-modal.appendChild(close_modal).innerHTML = 'fechar'.toUpperCase()
-
-
+const close_modal = document.querySelector('[type="button"]')
+// modal.appendChild(close_modal).innerHTML = 'fechar'.toUpperCase()
 
 open_modal.addEventListener('click', (e) => {
     modal.showModal()
 })
-close_modal.onclick = () => {
+close_modal.addEventListener('click', (e) => {
     modal.close()
-}
+})
 
